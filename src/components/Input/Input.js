875aaf2ef684559ef0fd0a01149bad45d67a3516
Rodrigo Lourenço <rsilvape@gmail.com>
2019-07-14 15:02:36 -0300

    Config styled component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Input = () => <h1>Input</h1>;
+import { Wrapper, Image, InputWrapper, ImageWrapper } from "./styles";
+
+const Input = ({ ...props }) => (
+  <Wrapper>
+    <ImageWrapper>
+      <Image src="/static/search.svg" />
+    </ImageWrapper>
+    <InputWrapper {...props} />
+  </Wrapper>
+);
 
 export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: #b5bcca;
+  width: 761px;
+  height: 112px;
+  border-radius: 56px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 0px 12px rgba(26, 26, 26, 0.5);
+`;
+
+export const ImageWrapper = styled.div`
+  padding-left: 16px;
+`;
+
+export const Image = styled.img`
+  width: 60px;
+`;
+
+export const InputWrapper = styled.input`
+  border: 0;
+  background: transparent;
+`;
